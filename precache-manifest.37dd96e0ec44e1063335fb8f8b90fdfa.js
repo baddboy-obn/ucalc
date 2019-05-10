ed4c04aaec47d4157ab7e8a4c4426b2d3420bd66
Konstantin <relict.adm@gmail.com>
2019-05-10 20:29:11 +0300

    Updates

--- precache-manifest.37dd96e0ec44e1063335fb8f8b90fdfa.js
+++ precache-manifest.37dd96e0ec44e1063335fb8f8b90fdfa.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/ucalc/static/js/runtime~main.5d151564.js"
   },
   {
-    "revision": "753be3a6485b2352c411",
-    "url": "/ucalc/static/js/main.753be3a6.chunk.js"
+    "revision": "9a6c5bc9583711065e19",
+    "url": "/ucalc/static/js/main.9a6c5bc9.chunk.js"
   },
   {
     "revision": "5553f5d96b40cd44b48f",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/ucalc/static/css/2.856abb13.chunk.css"
   },
   {
-    "revision": "95183aef5637aa24445bf6e3a9587c52",
+    "revision": "8e4b58efac88cacaf5801f8d51cdb6ce",
     "url": "/ucalc/index.html"
   }
 ];
